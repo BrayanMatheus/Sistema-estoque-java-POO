e455c6d27cbdf9fceba046a601834aa6148a2b87
B Matheus <matheussantos1924@hotmail.com>
2026-09-15 21:02:54 -0300

    Adiciona o metodo alterar e termina o CRUD

--- src/App.java
+++ src/App.java
@@ -14,7 +14,8 @@ public class App {
         System.out.println("[1] Cadastrar produto");
         System.out.println("[2] Mostrar todos os produtos");
         System.out.println("[3] Buscar produto (ID)");
-        System.out.println("[4] Excluir produto");
+        System.out.println("[4] Alterar produto");
+        System.out.println("[5] Excluir produto");
         System.out.println("[0] SAIR");
         System.out.println("\n");
         
@@ -50,7 +51,7 @@ public class App {
         System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
     }
 
-    public static void buscarProdutos(ArrayList<Produto> listaProdutos, Scanner scanner){
+    public static void buscarProduto(ArrayList<Produto> listaProdutos, Scanner scanner){
         Boolean encontrado = false;
         
         System.out.print("Digite o ID do produto desejado: ");
@@ -67,39 +68,141 @@ public class App {
         }
     }
 
-     public static void excluirProduto(ArrayList<Produto> listaProdutos, Scanner scanner){
+    
+    public static void excluirProduto(ArrayList<Produto> listaProdutos, Scanner scanner){
         
         System.out.print("Informe o id do produto para excluir: ");
         int idExcluir = scanner.nextInt();
-
+        
         Boolean encontrado = false;
         for (Produto produto : listaProdutos) {
             if (produto.getId() == idExcluir) {
                 listaProdutos.remove(produto);
                 encontrado = true;
+                break;
             }
         }
         if (!encontrado) {
             System.err.println("Produto não encotrado!");
         }
     }
+    
+    public static void alterarProduto(ArrayList<Produto> listaProdutos, Scanner scanner){
+        int alteracao = 0;
+        Boolean encontrado = false;
+        
+        System.out.print("Digite o ID do produto para alterar: ");
+        int idBuscado = scanner.nextInt();
 
+        for (Produto produto : listaProdutos) {
+            if (produto.getId() == idBuscado) {
+                encontrado = true;
+                produto.detalharProduto();
+                
+                do {
+                    System.out.println("\n");
+                    System.out.println("[1] NOME");
+                    System.out.println("[2] DEPARTAMENTO");
+                    System.out.println("[3] CATEGORIA");
+                    System.out.println("[4] VALOR");
+                    System.out.println("[5] QUANTIDADE");
+                    System.out.println("[6] TUDO");
+                    System.out.print("oque deseja alterar: ");
+                    alteracao = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("\n");
+
+                    switch (alteracao) {
+                        case 1:{
+                            // alteração NOME
+                            System.out.println("Digite o novo NOME do produto: ");
+                            String novoNome = scanner.nextLine();
+                            produto.setNome(novoNome);
+                            System.out.println("NOME Alterado com Sucesso!");
+                            System.out.println("\n");
+                            break;
+                        }
+                        case 2:{
+                            // alteração DEPARTAMENTO
+                            System.out.println("Digite o novo DEPARTAMENTO do produto: ");
+                            String novoDepartamento = scanner.nextLine();
+                            produto.setDepartamento(novoDepartamento);
+                            System.out.println("DEPARTAMENTO Alterado com Sucesso!");
+                            System.out.println("\n");
+                            break;
+                        }
+                        case 3:{
+                            // alteração CATEGORIA
+                            System.out.println("Digite a nova CATEGORIA do produto: ");
+                            String novaCategoria = scanner.nextLine();
+                            produto.setCategoria(novaCategoria);
+                            System.out.println("CATEGORIA Alterada com Sucesso!");
+                            System.out.println("\n");
+                            break;
+                        }
+                        case 4:{
+                            // alteração VALOR
+                            System.out.println("Digite o novo VALOR do produto: ");
+                            double novoValor = scanner.nextDouble();
+                            produto.setValor(novoValor);
+                            System.out.println("VALOR Alterado com Sucesso!");
+                            System.out.println("\n");
+                            break;
+                        }
+                        case 5:{
+                            // alteração QUANTIDADE
+                            System.out.println("Digite a nova QUANTIDADE do produto: ");
+                            int novaQuantidade = scanner.nextInt();
+                            produto.setQuantidade(novaQuantidade);
+                            System.out.println("QUANTIDADE Alterada com Sucesso!");
+                            System.out.println("\n");
+                            break;
+                        }
+                        case 6:{
+                            // alteração TODOS OS ATRIBUTOS
+                            System.out.print("Digite o novo NOME do produto: ");
+                            String novoNome = scanner.nextLine();
+                            produto.setNome(novoNome);
+
+                            System.out.print("Digite o novo DEPARTAMENTO do produto: ");
+                            String novoDepartamento = scanner.nextLine();
+                            produto.setDepartamento(novoDepartamento);
+
+                            System.out.print("Digite a nova CATEGORIA do produto: ");
+                            String novaCategoria = scanner.nextLine();
+                            produto.setCategoria(novaCategoria);
+
+                            System.out.print("Digite o novo VALOR do produto: ");
+                            double novoValor = scanner.nextDouble();
+                            produto.setValor(novoValor);
+
+                            System.out.print("Digite a nova QUANTIDADE do produto: ");
+                            int novaQuantidade = scanner.nextInt();
+                            produto.setQuantidade(novaQuantidade);
+
+                            System.out.println("PRODUTO Alterado com Sucesso!");  
+                            System.out.println("\n");
+                            alteracao = 0;
+                            break;
+                        }
+                        default:
+                            System.err.println("Opção não identificada...");
+                            break;
+                    }
+                } while (alteracao != 0);
+                break;
+            }
+        }
+        if (!encontrado) {
+            System.err.println("Produto não encotrado!");
+        }
+    }
+    
+    
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Produto> listaProdutos = new ArrayList<>();
         int opcao = 0;
-
-        // ---------------- CÓDIGOS PARA TESTES ----------------------------------------
-        // Produto produto1 = new Produto("aaaaaaaaaaaaa", "fsfdsfewf", "fsefsdfswe", 55, 9);
-        // Produto produto2 = new Produto("bbbbbbbbbbbb", "aaaaaaaa", "aaaa", 77, 3);
-        // Produto produto3 = new Produto("ccccccccc", "bbbbbb", "bbbbbbbbbb", 95, 3);
-        // Produto produto4 = new Produto("dddddddddddd", "dsadas", "ubt", 95, 3);
-        // Produto produto5 = new Produto("eeeeeeeeeeee", "ujty", "dgsrgrsh", 95, 3);
-        // listaProdutos.add(produto1);
-        // listaProdutos.add(produto2);
-        // listaProdutos.add(produto3);
-        // listaProdutos.add(produto4);
-        // listaProdutos.add(produto5);
 
         do {
             exibeMenu();
@@ -123,10 +226,14 @@ public class App {
                     break;
                 
                 case 3:
-                    buscarProdutos(listaProdutos, scanner);
+                    buscarProduto(listaProdutos, scanner);
                     break;
 
                 case 4:
+                    alterarProduto(listaProdutos, scanner);
+                    break;
+
+                case 5:
                     excluirProduto(listaProdutos, scanner);
                     break;
 
@@ -134,6 +241,6 @@ public class App {
                     System.err.println("Opção não identificada");
                     break;
             }
-        } while (opcao >= 0 && opcao <=3);
+        } while (opcao !=0);
     }
 }
